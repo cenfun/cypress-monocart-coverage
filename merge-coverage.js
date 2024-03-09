4df6c46c19eb252b3106bb6df3e3f70089f5a73f
cenfun <cenfun@gmail.com>
2024-03-09 09:37:57 +0800

    fix if no coverage cache

--- merge-coverage.js
+++ merge-coverage.js
@@ -1,16 +1,26 @@
-
+const fs = require('fs');
 const MCR = require('monocart-coverage-reports');
 
 const merge = async () => {
+
+    const inputDir = [
+        './coverage-reports/e2e/raw',
+        './coverage-reports/component/raw'
+    ];
+
+    // check input dir first
+    for (const rawDir of inputDir) {
+        if (!fs.existsSync(rawDir)) {
+            console.log(`Skip merging coverage. The raw dir is not exist: ${rawDir}`);
+            return;
+        }
+    }
 
     const coverageReport = MCR({
         // logging: 'debug',
         name: 'My Cypress merged Coverage Report',
 
-        inputDir: [
-            './coverage-reports/e2e/raw',
-            './coverage-reports/component/raw'
-        ],
+        inputDir,
         outputDir: './coverage-reports/merged',
         sourcePath: {
             'cypress-monocart-coverage/': ''
@@ -18,7 +28,17 @@ const merge = async () => {
         reports: [
             'v8',
             'console-details'
-        ]
+        ],
+
+        onEnd: () => {
+            // remove the raw files if it useless
+            inputDir.forEach((rawDir) => {
+                fs.rmSync(rawDir, {
+                    recursive: true,
+                    force: true
+                });
+            });
+        }
     });
 
     await coverageReport.generate();
